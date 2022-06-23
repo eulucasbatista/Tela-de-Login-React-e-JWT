3c51d9cab100970cba0aacdd498acbcc880283b3
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2022-06-23 17:18:23 -0300

    Updates
    Updates

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
-import logoIMG from'./assets/jp.svg'
+import { AppRouter } from "./routes";
+import "./assets/global.css";
 
-import './styles.css'
-
-
-function App() {
+export const App = () => {
   return (
-<div className="container">
-  <div className="container-login">
-    <div className="wrap-login">
-      <form className="login-form">
-        <span className="login-form-title">Bem Vindo!</span>
-
-        <span className="login-form-title">
-          <img src={logoIMG} alt="Logo Code" />
-        </span>
-        <div className="wrap-input">
-          <input className="input" type="email"></input>
-          <span className="focus-input" data-placeholder="Email"></span>
-        </div>
-
-        <div className="wrap-input">
-        <input className="input" type="password"></input>
-        <span className="focus-input" data-placeholder="Password"></span>
-        </div>
-
-        <div className="container-login-form-btn">
-          <button className="login-form-btn">Login</button>
-        </div>
-
-        <div className="text-center">
-          <span className="txt1">Não possui conta</span>
-
-          <a className="txt2" href="#">Criar conta.</a>
-        </div>
-
-      </form>
-    </div>
-  </div>
-</div>
+    <AppRouter />
   );
 }
-
-export default App;
