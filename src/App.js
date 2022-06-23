73c84d88b8525d506617695c89b39a8d0f340a34
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2022-06-23 14:43:11 -0300

    Update App.js
    New Features

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import logoIMG from'./assets/logo.svg'
+
 
 function App() {
   return (
@@ -5,10 +7,25 @@ function App() {
   <div className="container-login">
     <div className="wrap-login">
       <form className="login-form">
-        <span className="login-form-title"></span>
+        <span className="login-form-title">Bem Vindo!</span>
+
         <span className="login-form-title">
-          <img src={} alt="" />
+          <img src={logoIMG} alt="" />
         </span>
+    <div className="wrap-input">
+      <input type="email"></input>
+      <span className="focus-input" data-placeholder="Email"></span>
+    </div>
+
+    <div className="wrap-input">
+      <input type="password"></input>
+      <span className="focus-input" data-placeholder="Password"></span>
+    </div>
+
+   <div className="container-login-form-">
+    
+    </div>
+
       </form>
     </div>
   </div>
